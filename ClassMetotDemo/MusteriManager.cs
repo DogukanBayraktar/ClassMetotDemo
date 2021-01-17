84a4a05f887df72c09bf0f14cbbe02eb34832694
doguk <doguk@DESKTOP-L3I9NHT>
2021-01-17 22:52:04 +0300

    Ödev

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri) 
+        {
+            Console.WriteLine("Müşteri Eklendi."+"\n"+"Müşterimizin Adı : " + musteri.Ad + "\n" + "Müşterimizin Soyadı : " +musteri.Soyad+ "\n"+"Müşterinin Parası :" +musteri.HesapBakiyesi);
+        }
+        public void MusteriSil(Musteri[] liste) 
+        {
+            foreach (var musteri in liste)
+            {
+                Console.WriteLine("Eskiden Müşterimiz : " + musteri.Ad + " " + musteri.Soyad);
+            }
+        }
+        public void Liste(Musteri[]liste) 
+        { 
+            foreach(var musteri in liste) 
+            {
+                Console.WriteLine("Müşterilerimizin Listesi. Müşterimizin Adı : " + musteri.Ad + " Müşterimizin Soyadı : " + musteri.Soyad);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // Musteri classıda burda geliyor
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Dogukan";
+            musteri1.Soyad = "Bayraktar";
+            musteri1.HesapBakiyesi = 500;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Ceren";
+            musteri2.Soyad = "Bayraktar";
+            musteri2.HesapBakiyesi = 1500;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Ad = "Drogba";
+            musteri3.Soyad = "Bayraktar";
+            musteri3.HesapBakiyesi = 1500;
+
+            Musteri[] musteriler = new Musteri[] { musteri1,musteri2,musteri3};
+
+            //MusterManagerı burda cagırıyoruz
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriEkle(musteri2);
+            musteriManager.MusteriEkle(musteri3);
+            Console.WriteLine("-----------");
+            Console.WriteLine("Listede olan bütün müşterilerimiz");
+            musteriManager.Liste(musteriler);
+            Console.WriteLine("-----------");
+            Console.WriteLine("Silinen Müşteriler");
+            musteriManager.MusteriSil(musteriler);
+        }
+    }
+}
